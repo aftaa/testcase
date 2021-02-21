78fa4a35e33da2e3663b1884c7831d7ff06b3416
Maxim Gabidullin <after@ya.ru>
2021-02-21 04:15:10 +0300

    currencyNsi.php

---
+++ back/currencyNsi.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace back;
+
+const URL = 'https://www.cbr-xml-daily.ru/daily_json.js';
+
+$inputJson = file_get_contents(URL);
+$inputJson = json_decode($inputJson);
+
+
+$currencies = $inputJson->Valute;
+
+$outputJson = (object)[
+    'header' => [
+        'id',
+        'name',
+    ],
+    'data'   => [
+
+    ]
+];
+
+foreach ($currencies as $currency) {
+    $outputJson->data[] = [
+        $currency->NumCode,
+        $currency->Name,
+    ];
+}
+
+$outputJson = json_encode($outputJson);
+
+echo $outputJson;
